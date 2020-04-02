2006aa9aab3de92a6da5f7fd1080c149612fca3c
Hicham <tony.theedev@gmail.com>
2020-04-02 01:57:28 +0100

    create/insert

--- DataTools/LuxeDatabasePostgreSQL.sql
+++ DataTools/LuxeDatabasePostgreSQL.sql
@@ -1,1 +1,334 @@
-﻿
+﻿-- Obligatoir : PostgreSql 9.5
+-- En cas de problm .. ou si vous voulez supprimer la base de donnée mn serveur Postgre ..
+-- 1-- selectionnez "postgre" f DropDownList dyl Aure data studio ^
+-- 2- executez had les 3 requéte une par une.
+
+select *
+from pg_stat_activity
+where datname = 'pgluxedatabase';
+
+select pg_terminate_backend (pg_stat_activity.pid)
+from pg_stat_activity
+where pg_stat_activity.datname = 'pgluxedatabase';
+-- suprime avec succes = true
+drop database PgLuxeDatabase
+
+
+-- Creer la BD 
+create database PgLuxeDatabase
+-- !!! ==> selectionnez "PgLuxeDatabase" f DropDownList dyl Aure data studio ^
+
+create table SEXE
+(
+    id_sexe int primary key,
+    libelle_sexe varchar(10)
+);
+
+create table GRADE
+(
+    id_grade int primary key,
+    libelle_grade varchar(15)
+);
+
+create sequence PERSONNE_seq
+start 1000;
+create table PERSONNE
+(
+    id_client int default nextval ('PERSONNE_seq') primary key,
+    nom varchar(50),
+    prenom varchar(50),
+    sexe_ int ,
+    foreign key (sexe_)references SEXE(id_sexe),
+    est_Marie boolean,
+    nbr_Enfant_scolarise int,-- max 4
+    check(nbr_Enfant_scolarise <=4),
+    nbr_Enfant_non_scolarise int,--max 4
+    check(nbr_Enfant_non_scolarise <=4),
+    grade_ int,
+    foreign key (grade_)references GRADE(id_grade),
+    date_naissance date,
+    point_personne int
+    -- TODO : necessite un trigger.
+);
+
+create sequence REMARQUE_CLIENT_seq
+start 1;
+create table REMARQUE_CLIENT
+(
+    id_remarque int default nextval ('REMARQUE_CLIENT_seq') primary key,
+    personne_id int,
+    foreign key (personne_id) references PERSONNE(id_client),
+    description_remarque varchar(100)
+);
+
+create table AUTH_ROLE
+(
+    id_role int primary key,
+    description_role varchar(500),
+    libelle_role varchar(15)
+);
+
+create table AUTH_ROLE_PERSONNE
+(
+    personne_role_ int ,
+    auth_role_ int ,
+    primary key(personne_role_,auth_role_),
+    username_email varchar(50),
+    mot_de_passe varchar(100)
+);
+
+create table TYPE_LOGEMENT
+(
+    id_type_logement int primary key,
+    libelle_type_logement varchar(50)
+);
+
+create sequence DETAIL_LOGEMENT_seq
+start 1;
+create table DETAIL_LOGEMENT
+(
+    id_detail int default nextval('DETAIL_LOGEMENT_seq') primary key,
+    est_categorie BOOLEAN default false,
+    type_logement_ int,
+    foreign key (type_logement_)references TYPE_LOGEMENT(id_type_logement),-- villa/appartement/suite/chambre hotel
+    superficie_logement double precision,-- en metre carre
+    nbr_piece int,
+    capacite_personne_max int,
+    tarif_par_nuit_HS double precision,-- en dirham -- Haute saison
+    tarif_par_nuit_BS double precision,-- en dirham -- basse saison
+    description_logement varchar(1500),
+    max_reserv int,-- nbr maximum de reservation par saison (Haute/Basse saison)
+    tarif_annulation double precision,
+    marge_annulation int,
+    piscine_disponible BOOLEAN,
+    parking_disponible BOOLEAN,
+    jardin_cours BOOLEAN,
+    massage_disponible BOOLEAN
+);
+	
+create sequence LOGEMENT_seq
+start 1;
+create table LOGEMENT
+(
+    id_logement int default nextval ('LOGEMENT_seq') primary key,
+    nom_logement varchar(50),
+    detail_logement_ int,
+    foreign key (detail_logement_) references DETAIL_LOGEMENT,
+    adress_logement varchar(100)
+);
+
+create sequence PHOTO_LOGEMENT_seq
+start 1;
+create table PHOTO_LOGEMENT
+(
+    id_photo int default nextval ('PHOTO_LOGEMENT_seq') primary key,
+    chemin_photo varchar(250),
+    logement_ int,
+    foreign key (logement_) references LOGEMENT(id_logement)
+);
+
+create sequence PLANNING_LOGEMENT_seq
+start 1;
+create table PLANNING_LOGEMENT
+(
+    id_planing int default nextval ('PLANNING_LOGEMENT_seq') primary key,
+    logement_ int,
+    foreign key(logement_) references LOGEMENT(id_logement),
+    est_disponible boolean,--status du logement (en travaux/disponible) <false/true>
+    date_debut date,-- pas du tous obligatoires ... juste au cas au la maison est en etat de reparation ...
+    date_fin date-- pas du tous obligatoires ... juste au cas au la maison est en etat de reparation ...
+
+);
+
+create sequence DEMANDE_RESERVATION_seq
+start 1;
+create table DEMANDE_RESERVATION
+(
+    id_demande int default nextval ('DEMANDE_RESERVATION_seq') primary key,
+    date_demande date,
+    personne_ int ,
+    foreign key (personne_)references PERSONNE(id_client),
+    logement_ int,
+    foreign key(logement_) references LOGEMENT(id_logement),
+    date_debut date,
+    date_fin date,
+    annule_par_client boolean default 'false',
+    date_annulation date
+);
+
+create sequence RESERVATION_LOGEMENT_seq
+start 1;
+create table RESERVATION_LOGEMENT
+(
+    id_reservation int default nextval ('RESERVATION_LOGEMENT_seq') primary key,
+    demande_reservation_ int,
+    foreign key (demande_reservation_)references DEMANDE_RESERVATION(id_demande)
+);
+
+create table FACTURATION
+(
+    id_facture int primary key,
+    reservation_logement_ int,
+    foreign key(reservation_logement_) references RESERVATION_LOGEMENT(id_reservation),
+    note_client int,-- L’echelle de satisfaction du collaborateur (0-5 etoile)
+    check(note_client >=0 and note_client <=5),
+    commentaire_client varchar(250)
+);
+
+create sequence SAUVEGARDE_LOGEMENT_seq
+start 1;
+create table SAUVEGARDE_LOGEMENT
+(
+    id_sauvegarde int default nextval ('SAUVEGARDE_LOGEMENT_seq') primary key,
+    client_ int,
+    foreign key (client_) references PERSONNE(id_client),
+    logement_ int,
+    foreign key (logement_)references LOGEMENT(id_logement)
+);
+
+create sequence MESSAGE_seq
+start 1;
+create table MESSAGE_CONTACT
+(
+    id_message int default nextval ('MESSAGE_seq') primary key,
+    emetteur_ int,
+    foreign key (emetteur_) references PERSONNE(id_client),
+    Message_ecrit CHARACTER(250),
+    vu boolean default 'false',
+    recepteur_ int,
+    foreign key (recepteur_) references PERSONNE(id_client)
+);
+
+-- insert AUTH_ROLE
+INSERT into AUTH_ROLE (id_role, description_role, libelle_role) 
+VALUES 
+(1, N'Votre tache principale est de gerer le sytem et veiller a la securite des donnees clientel.', N'Admin'),
+(2, N'Vous pouvez reserver et profitez de toute nos offres clientele ! en plus vous etes base sur un systeme de points qui vous qualifiera aussitot que possible au moment de la reservation, soyez toujous les merveilleux bienvenues !', N'SimpleUser');
+
+-- insert GRADE
+INSERT into GRADE (id_grade, libelle_grade) 
+VALUES 
+(0, N'Administratif') ,
+(1, N'Directeur')	  ,
+(2, N'Employé/cadre.'),
+(3, N'Retraité')	  ;
+
+-- insert SEXE
+INSERT into SEXE (id_sexe, libelle_sexe) 
+VALUES 
+(1, N'Homme'),
+(2, N'Femme');
+
+-- insert PERSONNE
+INSERT into PERSONNE (id_client, nom, prenom, sexe_, est_marie, nbr_enfant_scolarise, nbr_enfant_non_scolarise, grade_, date_naissance, point_personne) 
+VALUES 
+(1000, N'Sqat', N'Amina', 2, FALSE, 0, 0, 2, CAST(N'1998-08-07' AS Date), 0) ,
+(1002, N'Tfaal', N'Omaima', 2, FALSE, 0, 0, 2, CAST(N'1998-12-08' AS Date), 0),
+(1003, N'Ofdil', N'Ihssane', 2, TRUE, 2, 1, 2, CAST(N'1990-02-02' AS Date), 0),
+(1004, N'Filali', N'Nisrine', 2, FALSE, 1, 1, 1, CAST(N'1985-09-05' AS Date), 0),
+(1005, N'Chennouf', N'Hicham', 1, FALSE, 0, 0, 2, CAST(N'1995-04-06' AS Date), 0),
+(1006, N'Lhlou', N'Nourdine', 1, TRUE, 1, 2, 1, CAST(N'1997-02-19' AS Date), 0),
+(1007, N'Brahimi', N'Mohmmed', 1, TRUE, 0, 1, 2, CAST(N'1983-09-09' AS Date), 0),
+(1008, N'Issaoui', N'Ahmed', 1, TRUE, 3, 0, 3, CAST(N'1970-03-05' AS Date), 0),
+(1009, N'Toufiq', N'Ihab', 1, TRUE, 0, 0, 1, CAST(N'1990-07-20' AS Date), 0),
+(1010, N'Biwi', N'israe', 2, FALSE, 0, 0, 2, CAST(N'2000-11-05' AS Date), 0),
+(1011, N'Hmouti', N'Nihal', 2, FALSE, 2, 0, 2, CAST(N'1992-08-25' AS Date), 0),
+(1012, N'O-Sfh', N'Hicham', 1, FALSE, 0, 0, 0, CAST(N'1997-08-25' AS Date), 0),
+(1013, N'Yaagoubi', N'Nourdine', 1, FALSE, 0, 0, 0, CAST(N'1998-02-02' AS Date), 0),
+(1014, N'Ziane', N'Mohmmed', 1, FALSE, 0, 0, 0, CAST(N'1996-09-02' AS Date), 0),
+(1015, N'Yagobi', N'Ahmed', 1, FALSE, 0, 0, 0, CAST(N'1998-06-26' AS Date), 0);
+
+-- insert AUTH_ROLE_PERSONNE
+INSERT into AUTH_ROLE_PERSONNE (personne_role_, auth_role_, username_email, mot_de_passe) 
+VALUES 
+(1000, 2, N'mail1@gmail.com', N'MotDePasse1') ,
+(1002, 2, N'mail2@gmail.com', N'MotDePasse2') ,
+(1003, 2, N'mail3@gmail.com', N'MotDePasse3') ,
+(1004, 2, N'mail4@gmail.com', N'MotDePasse4') ,
+(1005, 2, N'mail5@gmail.com', N'MotDePasse5') ,
+(1006, 2, N'mail6@gmail.com', N'MotDePasse6') ,
+(1007, 2, N'mail7@gmail.com', N'MotDePasse7') ,
+(1008, 2, N'mail8@gmail.com', N'MotDePasse8') ,
+(1009, 2, N'mail9@gmail.com', N'MotDePasse9') ,
+(1010, 2, N'mail10@gmail.com', N'MotDePasse10'),
+(1011, 2, N'mail11@gmail.com', N'MotDePasse11'),
+(1012, 1, N'mail12@gmail.com', N'MotDePasse12'),
+(1013, 1, N'mail13@gmail.com', N'MotDePasse13'),
+(1014, 1, N'mail14@gmail.com', N'MotDePasse14'),
+(1015, 1, N'mail15@gmail.com', N'MotDePasse15');
+
+-- insert MESSAGE_CONTACT
+INSERT into MESSAGE_CONTACT (id_message, emetteur_, message_ecrit, vu, recepteur_) 
+VALUES 
+(1, 1012, N'Bonjour soyez le bienvenue ', FALSE, 1000),
+(2, 1013, N'Bonjour soyez le bienvenue ', FALSE, 1002),
+(3, 1014, N'Bonjour soyez le bienvenue ', TRUE, 1003),
+(4, 1015, N'Bonjour soyez le bienvenue ', FALSE, 1004),
+(5, 1013, N'Bonjour soyez le bienvenue ', TRUE, 1005),
+(6, 1015, N'Bonjour soyez le bienvenue ', FALSE, 1006),
+(8, 1014, N'Bonjour soyez le bienvenue ', FALSE, 1007),
+(9, 1012, N'Bonjour soyez le bienvenue ', FALSE, 1008),
+(10,1013, N'Bonjour soyez le bienvenue', TRUE, 1009),
+(11,1012, N'Bonjour soyez le bienvenue', FALSE, 1010),
+(12,1014, N'Bonjour soyez le bienvenue', FALSE, 1011);
+
+-- insert TYPE_LOGEMENT
+INSERT into TYPE_LOGEMENT (id_type_logement, libelle_type_logement)
+VALUES 
+(1, N'Villa') 			   ,
+(2, N'Appartement')		   ,
+(3, N'Maison')			   ,
+(4, N'Chambre Hotel Luxe') ,
+(5, N'Chambre Hotel Sympa');
+
+-- insert DETAIL_LOGEMENT
+INSERT into DETAIL_LOGEMENT (id_detail, est_categorie, type_logement_, superficie_logement, nbr_piece, capacite_personne_max, tarif_par_nuit_hs, tarif_par_nuit_bs, description_logement, max_reserv, tarif_annulation, marge_annulation, piscine_disponible, parking_disponible, jardin_cours, massage_disponible) 
+VALUES 
+ (1, true, 5, 95, 3, 3, 350, 200, 'Des murs lambrissés en chêne, des volets en bois et un parquet chaleureux vous entourent. Faites votre choix parmi des centaines de livres et lisez-les dans une boîte à lit secrète derrière des étagères allant du sol au plafond. Les portraits de famille et les objets de famille cohabitent avec bonheur avec des œuvres d''art moderne.', 20, 150, 10, false, true, false, false);
+ (3, false, 3, 300, 4, 6, 1000, 700, 'Faites une expérience hors du temps, dans cette magnifique maison marocaine traditionnelle avec sa piscine et ses jardins privatifs. Idéale pour une famille ou des amis, elle vous permettra de vous détendre dans un cadre élégant et épuré.', 3, 500, 10, true, true, true, false);
+ (2, true, 1, 200, 4, 5, 1570, 1150, 'Un lieu exceptionnel idéalement situé. Tout se fait facilement à pieds. Une chambre magnifique, une terrasse des plus agréables, une piscine chauffée. La nourriture succulente ! Et quel accueil ! Une équipe aux petits soins qui prend le temps de tout vous expliquer et vous conseiller. Le hammam, un bonheur. Bref, tout était plus que parfait.', 50, 450, 31, true, true, true, true);
+ (4, true, 2, 150, 3, 4, 600, 470, 'Appartement panoramique Medina', 10, 200, 10, false, true, true, true);
+ (5, false, 2, 270, 4, 6, 680, 550, 'Notre maison cosy, colorée et confortable, située dans une très vieille maison traditionnelle de la médina, est disponible pour tous ceux qui souhaitent approfondir la sensation de fraîcheur d’Essaouira et retrouver la tranquillité d’esprit en cette période de forte activité. Vous avez votre espace totalement privé comprenant une chambre avec lit double, un salon, une petite cuisine, une salle de bain et un grand hall ressemblant à un patio, mais nous sommes prêts à socialiser si vous le souhaitez!', 15, 700, 15, true, false, true, false);
+ (6, true, 4, 180, 3, 2, 1100, 980, 'Studio charifa une chambre dans riad diamant blanc au 2 eme étage la chambre a une vue sur mer et le jardin .', 0, 600, 7, true, true, true, true);
+
+-- insert LOGEMENT
+INSERT into LOGEMENT (id_logement, nom_logement, detail_logement_, adress_logement)
+VALUES 
+(2, 'Hotel Tildi', 1, 'Rue Hubert Giraud, Ville Nouvelle - 80000 Agadir');
+(3, 'Hotel Tildi', 1, 'Rue Hubert Giraud, Ville Nouvelle - 80000 Agadir');
+(1, 'Dar Itrane - Superbe maison berbère', 3, 'Tagadirt, Marrakech-Tensift-Al Haouz, Maroc');
+(4, 'Villa Authentique Medina', 2, 'Marrakech, Tensift, Maroc');
+(5, 'Villa Authentique Medina', 2, 'Marrakech, El Haouz, Maroc');
+(6, 'Appartement panoramique', 4, 'Marrakech, entre Tensift et El Haouz, Maroc');
+(7, 'Appartement panoramique', 4, 'Marrakech, entre Tensift et El Haouz, Maroc');
+(8, 'Appartement panoramique', 4, 'Marrakech, entre Tensift et El Haouz, Maroc');
+(9, 'Appartement paisible ', 5, 'Essaouira, Marrakech-Safi, Maroc');
+(11, 'Studio charifa', 6, 'Fes, Boulvard Asslat, Maroc');
+(10, 'Studio charifa double vue mer', 6, 'Fes, environs de quartier El Mouahidine, Maroc');
+
+-- insert REMARQUE_CLIENT
+INSERT into REMARQUE_CLIENT (id_remarque, personne_id, description_remarque) 
+VALUES
+(1, 1000, 'Prend bien soin du logement.');
+(2, 1010, 'lay3merha dar.');
+(3, 1008, 'Il a cassé 2 vase.');
+
+-- insert SAUVEGARDE_LOGEMENT
+INSERT into SAUVEGARDE_LOGEMENT (id_sauvegarde, client_, logement_) 
+VALUES 
+(2, 1000, 5);
+(3, 1000, 2);
+(4, 1005, 3);
+
+--insert PHOTO_LOGEMENT
+INSERT
+
+
+
+--Pour faire backup : aller dans  => C:\Program Files\PostgreSQL\9.5\bin
+-- commande : pg_dump --column-inserts -U postgres -a -p 5433 -d pgluxedatabase > C:\Users\pc\Desktop\testBackup.sql
+-- -U : postgres : Username 
+-- -d : pgluxedatabse : database name 
+-- -a : generer que le script insert
+-- -p : port : 5433
+-- > chemin du Ficier Backup (extension .sql/.backup obligatoire).
