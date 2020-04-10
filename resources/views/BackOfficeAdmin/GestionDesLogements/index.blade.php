c299fedf27e5ad28d0810d0715ef6e212edfd11f
Hicham <tony.theedev@gmail.com>
2020-04-10 17:03:33 +0100

    Hicham

--- resources/views/BackOfficeAdmin/GestionDesLogements/index.blade.php
+++ resources/views/BackOfficeAdmin/GestionDesLogements/index.blade.php
@@ -2,8 +2,22 @@
 
 <link rel="stylesheet" href="css/items.css" />
 <link rel="stylesheet" href="css/jumbotron.css" />
-<script src="https://kit.fontawesome.com/4f2d779e50.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="css/SearchRBNB.css" />
 
+<!-- Icons font CSS-->
+<link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all" />
+<link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all" />
+<!-- Font special for pages-->
+<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet" />
+
+<!-- Vendor CSS-->
+<link href="vendor/select2/select2.min.css" rel="stylesheet" media="all" />
+<link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all" />
+
+<!-- Main CSS-->
+<link href="css/main.css" rel="stylesheet" media="all" />
+
+<script src="https://kit.fontawesome.com/4f2d779e50.js" crossorigin="anonymous"></script>
 
 @section('title','Accueil')
 
@@ -14,6 +28,75 @@
     <i class="fa fa-plus" aria-hidden="true"></i>
     Nouveau Logement
 </a>
+<!--<form>
+    <table cellspacing="0" cellpadding="0">
+        <colgroup>
+            <col style="width:70%" />
+            <!--       <col style="width:10%">
+      <col style="width:10%">
+      <col style="width:5%"> 
+        </colgroup>
+        <tr>
+            <td>
+                <input type="text" name="search-td" placeholder=' test test ' class="search-td" maxlength="65" />
+            </td>
+
+            <td>
+                <button type="button" name="dates" class="date-button date-field">
+                    <i class="far fa-calendar-alt"></i>Date
+                </button>
+            </td>
+            <td>
+
+                <td>
+                    <button type="button" name="guests" class="guests-button">
+                        <i class="fas fa-users"></i>Personnes
+                    </button>
+                </td>
+                <td>
+                    <button type="submit" class="search-button">
+                        <i class="material-icons search-icon">Rechercher !</i>
+                    </button>
+                </td>
+        </tr>
+
+    </table>
+</form>-->
+
+<!--<div class="page-wrapper bg-color-1 p-t-395 p-b-120">
+-->    <!--<div class="wrapper wrapper--w1070">-->
+        <div class="cardCodePen cardCodePen-7">
+            <div class="cardCodePen-body">
+                <form class="form" method="POST" action="#">
+                    <div class="input-group input--large">
+                        <label class="label">going to</label>
+                        <input class="input--style-1" type="text" placeholder="Destination, hotel name" name="going" />
+                    </div>
+                    <div class="input-group input--medium">
+                        <label class="label">Check-In</label>
+                        <input class="input--style-1" type="text" name="checkin" placeholder="mm/dd/yyyy" id="input-start" />
+                    </div>
+                    <div class="input-group input--medium">
+                        <label class="label">Check-Out</label>
+                        <input class="input--style-1" type="text" name="checkout" placeholder="mm/dd/yyyy" id="input-end" />
+                    </div>
+                    <div class="input-group input--medium">
+                        <label class="label">guests</label>
+                        <div class="input-group-icon js-number-input">
+                            <div class="icon-con">
+                                <span class="plus">+</span>
+                                <span class="minus">-</span>
+                            </div>
+                            <input class="input--style-1 quantity" type="text" name="guests" value="2 Guests" />
+                        </div>
+                    </div>
+                    <button class="btn-submit" type="submit">search</button>
+                </form>
+            </div>
+        </div>
+    <!--</div>-->
+<!--</div>
+-->
 <!--<table class="table table-hover mt-3">
     <thead>
         <tr>
@@ -87,5 +170,18 @@
 
         }
     </script>
+
+    <!-- Jquery JS-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <script src="vendor/jquery-validate/jquery.validate.min.js"></script>
+    <script src="vendor/bootstrap-wizard/bootstrap.min.js"></script>
+    <script src="vendor/bootstrap-wizard/jquery.bootstrap.wizard.min.js"></script>
+    <script src="vendor/datepicker/moment.min.js"></script>
+    <script src="vendor/datepicker/daterangepicker.js"></script>
+
+    <!-- Main JS-->
+    <script src="js/global.js"></script>
 </div>
 @endsection
