c05781accae1d78d450401440b6c91bee2a9ac59
Mohammed <62032938+Znajib@users.noreply.github.com>
2020-04-11 20:01:39 +0100

    Add files via upload

---
+++ app/Message_contact.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message_contact extends Model
+{
+    //
+}
---
+++ app/Sauvegarde_logement.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sauvegarde_logement extends Model
+{
+    //
+}
