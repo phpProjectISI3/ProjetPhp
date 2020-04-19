0bdb51c774ba85c268037792febb624412800341
Hicham <tony.theedev@gmail.com>
2020-04-19 14:12:38 +0000

    refactorisation about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -90,10 +90,10 @@
 					<div class="hotel-content" >
 						<div class="hotel-grid"  style="background-image: url({{ DB::table('photo_logement')->join('logement','photo_logement.logement_','=','logement.id_logement')->select('photo_logement.chemin_photo')->where('logement.id_logement',$logement->id_logement)->value('chemin_photo')}});">
 							<div class="price" ><small></small><span>{{$logement->tarif_par_nuit_hs}}/nuit</span></div>
-							<a class="book-now text-center" href="/detailRecherche/{{$logement->id_logement}}"><i class="ti-calendar"></i> Réservé </a>
+							<a class="book-now text-center" href="/detailRecherche/{{$logement->id_logement}}"><i class="ti-calendar"></i> Réserver ! </a>
 						</div>
 						<div class="desc sameHeight" id="carte">
-							<h3><a href="{{url('/detailRecherche')}}">{{$logement->nom_logement}} </a></h3>
+							<h3><a href="/detailRecherche/{{$logement->id_logement}}">{{$logement->nom_logement}} </a></h3>
 							<p class="carte__description">{{$logement->description_logement}}</p>
 						</div>
 					</div>
@@ -106,70 +106,7 @@
 						divs[i].innerHTML = divs[i].innerHTML.substring(0, 110) + '....<a href="{{url('detailRecherche')}}" style="color: orangered;">plus de détail !</a>';
 					}
 				</script>
-				<!-- <div class="col-md-4">
-					<div class="hotel-content">
-						<div class="hotel-grid" style="background-image: url(images/2.jpg);">
-							<div class="price"><small></small><span>$70/night</span></div>
-							<a class="book-now text-center" href="{{url('/detailRecherche')}}"><i class="ti-calendar"></i> Réservé</a>
-						</div>
-						<div class="desc">
-							<h3><a href="{{url('/detailRecherche')}}">Maison / Meknes</a></h3>
-							<p>Les Maisons sont décorées dans des tons chauds et raffinés et sont pourvues de la climatisation et
-							d'équipements à la pointe de la technologie.</p>
-						</div>
-					</div>
 				</div>
-				<div class="col-md-4">
-					<div class="hotel-content">
-						<div class="hotel-grid" style="background-image: url(images/3.jpg);">
-							<div class="price"><small></small><span>$200/night</span></div>
-							<a class="book-now text-center" href="{{url('/detailRecherche')}}"><i class="ti-calendar"></i> Réservé</a>
-						</div>
-						<div class="desc">
-							<h3><a href="{{url('/detailRecherche')}}">Appartement</a></h3>
-							<p>Appartement ne peut être classé 5 étoiles que si des critères précis sont honorés. Ces derniers sont divisés en trois
-							catégories : « équipements », « service au client » et « accessibilité et développement durable ».</p>
-						</div>
-					</div>
-				</div> -->
-				<!--<div class="col-md-4">
-					<div class="hotel-content">
-						<div class="hotel-grid" style="background-image: url(images/3.png);">
-							<div class="price"><small>For as low as</small><span>$100/night</span></div>
-							<a class="book-now text-center" href="#"><i class="ti-calendar"></i> Book Now</a>
-						</div>
-						<div class="desc">
-							<h3><a href="#">Hotel Name</a></h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-						</div>
-					</div>
-				</div>
-				 <div class="col-md-4">
-					<div class="hotel-content">
-						<div class="hotel-grid" style="background-image: url(images/telechargement1.jpg);">
-							<div class="price"><small>For as low as</small><span>$100/night</span></div>
-							<a class="book-now text-center" href="#"><i class="ti-calendar"></i> Book Now</a>
-						</div>
-						<div class="desc">
-							<h3><a href="#">Hotel Name</a></h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="hotel-content">
-						<div class="hotel-grid" style="background-image: url(images/image-6.jpg);">
-							<div class="price"><small>For as low as</small><span>$100/night</span></div>
-							<a class="book-now text-center" href="#"><i class="ti-calendar"></i> Book Now</a>
-						</div>
-						<div class="desc">
-							<h3><a href="#">Hotel Name</a></h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-						</div>
-					</div> -->
-				</div>
-
-			</div>
 		</div>
 	</div>
 
