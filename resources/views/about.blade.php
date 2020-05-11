5d2942f62488d1c5f428655aae84c199a36cff07
Hicham <tony.theedev@gmail.com>
2020-05-11 16:02:57 +0000

    link About.blade
    plus de detail

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -12,8 +12,7 @@
     <div class="overlay"></div>
     <div class="container">
         <div class="row">
-            <div
-                class="col-md-12 col-md-offset-0 col-sm-12 col-sm-offset-0 col-xs-12 col-xs-offset-0 text-center fh5co-table">
+            <div class="col-md-12 col-md-offset-0 col-sm-12 col-sm-offset-0 col-xs-12 col-xs-offset-0 text-center fh5co-table">
                 <div class="fh5co-intro fh5co-table-cell">
                     <h1 class="text-center">Choisissez Votre Offre</h1>
                     <p>La vie est à peu près la même sous toutes les latitudes.</p>
@@ -34,8 +33,8 @@
                     <section>
                         <select class="cs-select cs-skin-border selectManual">
                             @foreach($types as $type)
-                                <option value="{{ $type->id_type_logement }}">{{ $type->libelle_type_logement }}
-                                </option>
+                            <option value="{{ $type->id_type_logement }}">{{ $type->libelle_type_logement }}
+                            </option>
                             @endforeach
                         </select>
                     </section>
@@ -44,8 +43,8 @@
                     <section>
                         <select class="cs-select cs-skin-border selectManual">
                             @foreach($CapacitePersonne as $capacite)
-                                <option value="{{ $capacite->capacite_personne_max }}">{{ $capacite->capacite_personne_max }}
-                                </option>
+                            <option value="{{ $capacite->capacite_personne_max }}">{{ $capacite->capacite_personne_max }}
+                            </option>
                             @endforeach
                         </select>
                     </section>
@@ -54,9 +53,9 @@
                     <section>
                         <select class="cs-select cs-skin-border selectManual">
                             @foreach($villes as $ville)
-                                <option value="{{ $ville->adress_logement }}">
-                                    {{ $ville->adress_logement }}
-                                </option>
+                            <option value="{{ $ville->adress_logement }}">
+                                {{ $ville->adress_logement }}
+                            </option>
                             @endforeach
                         </select>
                     </section>
@@ -81,27 +80,27 @@
             @foreach($logements as $logement)
             <div class="col-md-4">
                 <div class="hotel-content">
-                    <div class="hotel-grid"
-                        style="background-image: url({{ DB::table('photo_logement')->join('logement','photo_logement.logement_','=','logement.id_logement')->select('photo_logement.chemin_photo')->where('logement.id_logement',$logement->id_logement)->value('chemin_photo')}});">
+                    <div class="hotel-grid" style="background-image: url({{ DB::table('photo_logement')->join('logement','photo_logement.logement_','=','logement.id_logement')->select('photo_logement.chemin_photo')->where('logement.id_logement',$logement->id_logement)->value('chemin_photo')}});">
                         <div class="price"><small></small><span>{{$logement->tarif_par_nuit_hs}}/nuit</span></div>
-                        <a class="book-now text-center" href="/detailRecherche/{{$logement->id_logement}}"><i
-                                class="ti-calendar"></i> Réserver </a>
+                        <a class="book-now text-center" href="/detailRecherche/{{$logement->id_logement}}"><i class="ti-calendar"></i> Réserver </a>
                     </div>
                     <div class="desc sameHeight" id="carte">
                         <h3><a href="/detailRecherche/{{$logement->id_logement}}">{{$logement->nom_logement}} </a></h3>
-                        <p class="carte__description">{{$logement->description_logement}}</p>
+                        <p id="description{{ $logement->id_logement }}" class="carte__description">{{$logement->description_logement}}</p>
+                        <script>
+                            var description = document.getElementById('description{{ $logement->id_logement }}');
+                            description.innerHTML = description.innerHTML.substring(0, 110) + '....<a href="/detailRecherche/{{$logement->id_logement}}" style="color: orangered;">plus de détail !</a>';
+                        </script>
                     </div>
                 </div>
                 @if(App\Http\Controllers\Auth_Role_PersonneController::IsAuthentificated())
                 <div style="margin-top: -19%;">
                     @if(DB::table('sauvegarde_logement')->where('sauvegarde_logement.client_',session()->get('userObject')->id_client)->where('sauvegarde_logement.logement_',$logement->id_logement)->exists())
-                    <input id="heart{{$logement->id_logement}}" class="heart" type="checkbox"
-                        onclick="AddToFavorite({{$logement->id_logement}})" checked />
+                    <input id="heart{{$logement->id_logement}}" class="heart" type="checkbox" onclick="AddToFavorite({{$logement->id_logement}})" checked />
                     @else
-                    <input id="heart{{$logement->id_logement}}" class="heart" type="checkbox"
-                        onclick="AddToFavorite({{$logement->id_logement}})" />
-					@endif
-					<label for="heart{{$logement->id_logement}}" class="label_heart">❤</label>
+                    <input id="heart{{$logement->id_logement}}" class="heart" type="checkbox" onclick="AddToFavorite({{$logement->id_logement}})" />
+                    @endif
+                    <label for="heart{{$logement->id_logement}}" class="label_heart">❤</label>
                 </div>
                 @endif
             </div>
@@ -122,26 +121,19 @@
 <script src="../js/jquery.js"></script>
 <script src="../js/notification/notify.min.js"></script>
 <script>
-    var i;
-    var divs = document.getElementsByClassName('carte__description');
-    for (i = 0; i < divs.length; i++) {
-        divs[i].innerHTML = divs[i].innerHTML.substring(0, 110) + '....<a href="{{url('detailRecherche ')}}" style="color: orangered;">plus de détail !</a>';
-    }
-
     $.ajaxSetup({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
 
-    function typeSelect(id_logement){
+    function typeSelect(id_logement) {
         var select = document.getElementById("")
     }
 
     function AddToFavorite(id_logement) {
         var checkBox = document.getElementById("heart" + id_logement);
         if (checkBox.checked == true) {
-            console.log("checked !");
             $.ajax({
                 url: "{{route('PagesController.favorit')}}",
                 method: "GET",
@@ -149,7 +141,7 @@
                     ID: id_logement
                 },
                 dataType: 'json',
-                success: function (text) {
+                success: function(text) {
                     $.notify(text.nomLogement + " : au favoris !", {
                         className: "success",
                         showDuration: 800,
@@ -168,7 +160,7 @@
                     ID: id_logement
                 },
                 dataType: 'json',
-                success: function (text) {
+                success: function(text) {
                     $.notify(text.nomLogement + " : suprimé des favoris !", {
                         className: "error",
                         showDuration: 800,
@@ -181,13 +173,11 @@
             });
         }
     }
-
 </script>
 <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
         $("a.active").removeClass();
         $("a#linkNosOffres").addClass('active');
     });
-
 </script>
 @endsection
